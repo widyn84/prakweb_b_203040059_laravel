59086abb68fcbf1277347e4cb1cf723f08254a94
widyn84 <widyn84@gmail.com>
2022-11-03 20:19:36 +0700

    Video 12 Redesign UI

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,22 @@
 @extends('layouts.main')
 
 @section('container')
-<article>
-<h1 class="mb-5">{{$post->title}}</h1>
+<div class="container">
+    <div class="row justify-content-center mb-5">
+        <div class="col-md-8">
+            <h1 class="mb-3">{{$post->title}}</h1>
+            <p>By. <a href="/authors/{{$post->author->username }} " class="text-decoration-none"> {{ $post->author->name    }}</a> In <a href="/categories/{{ $post->category->slug   }}" class="text-decoration-none"> {{ $post->category->name   }}</a></p>
+            
+            <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
 
-<p>By. <a href="/authors/{{$post->author->username }} " class="text-decoration-none"> {{  $post->author->name    }}</a> <a href="/categories/{{ $post->category->slug   }}" class="text-decoration-none"> {{ $post->category->name   }}</a></p>
+            <article class="my-3 fs-5">
+            {!! $post->body !!}
 
-{!! $post->body !!}
+            </article>
+            <a href="/blog" class="d-block mt-3">Back to Posts</a>
+        </div>
+    </div>
+</div>
 
-</article>
-<a href="/blog" class="d-block mt-3">Back to Posts</a>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
     ]);
 });
 
@@ -43,6 +43,7 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function() {
     return view ('categories', [
         'title' => 'Post Categories',
+        'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
@@ -50,6 +51,7 @@ Route::get('/categories', function() {
 Route::get('/categories/{category:slug}', function(Category $category) {
     return view ('posts', [
         'title' => "Post By Category: : $category->name",
+        'active' => 'categories',
         'posts' => $category->posts->load('category', 'author'),
 
     ]);
