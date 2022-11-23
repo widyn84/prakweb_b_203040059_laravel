3b11115f864a9b927324407ff20ed9dc1ea4883d
widyn84 <widyn84@gmail.com>
2022-11-23 21:15:51 +0700

    Video 19 Validation & Insert Post

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -5,6 +5,12 @@
     <h1 class="h2">My Posts</h1>
 </div>
 
+@if(session()->has('success'))
+<div class="alert alert-success" role="alert">
+    {{ session('success') }}
+</div>
+@endif
+
 <div class="table-responsive col-lg-8">
     <a href="/dashboard/posts/create" class="btn btn-primary mb-3">Create new post</a>
     <table class="table table-striped table-sm">
@@ -17,7 +23,7 @@
             </tr>
         </thead>
         <tbody>
-        @foreach ($posts as $post)
+            @foreach ($posts as $post)
             <tr>
                 <td>{{ $loop->iteration }}</td>
                 <td>{{ $post->title }}</td>
@@ -28,7 +34,7 @@
                     <a href="" class="badge bg-danger"><span data-feather="x-circle"></span></a>
                 </td>
             </tr>
-        @endforeach    
+            @endforeach
         </tbody>
     </table>
 </div>
